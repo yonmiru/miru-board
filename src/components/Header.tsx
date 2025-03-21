49298a6edb1e6f51670af247da19d654ba328780
jonzski <jhperez4@up.edu.ph>
2025-03-22 03:27:10 +0800

    Update `Header.tsx`

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return (
+      <header className="flex justify-center items-center p-12 gap-4">
+        <h1 className="text-5xl font-bold">YonMiru's Birthday</h1>
+      </header>
+    );
+  }
+  
