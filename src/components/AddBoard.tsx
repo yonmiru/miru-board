5199fa9a08dd57e797c94d56e208df10ad0d1933
jonzski <jhperez4@up.edu.ph>
2025-03-22 03:26:49 +0800

    Create Add Board button w/ dialog

---
+++ src/components/AddBoard.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/ui/dialog"
+import { Input } from "@/ui/input"
+import { Label } from "@/ui/label"
+import { Plus } from "lucide-react"
+
+export function AddBoard() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+      <div className="fixed bottom-10 right-10">
+        <Button variant="outline" size="lg">
+          <Plus className="h-6 w-6" />
+          <Label>Add Board</Label>
+        </Button>
+      </div>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
