f535cae857efd55af04b7f0fede124f6c7f4f96f
jonzski <jhperez4@up.edu.ph>
2025-03-23 04:16:07 +0800

    Put the footer on the right side

--- src/ui/card.tsx
+++ src/ui/card.tsx
@@ -75,11 +75,12 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      className={cn("flex justify-end px-6 [.border-t]:pt-6", className)}
       {...props}
     />
-  )
+  );
 }
+
 
 export {
   Card,
