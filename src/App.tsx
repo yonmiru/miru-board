8891ebcc7630fe98d27451fb72c17ea54cdd4eff
jonzski <jhperez4@up.edu.ph>
2025-03-22 22:42:00 +0800

    Import components on `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from "@/components/ThemeProvider"
 import Header  from "@/components/Header"
-import { ModeToggle } from "@/components/ModeToggle"
 import { AddBoard } from "@/components/AddBoard";
+import Canvas from "@/components/Canvas";
+import Board from "@/components/Board";
 
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Header />
-      <ModeToggle />
-      <AddBoard />
+      <div className="relative">
+        <Canvas />
+        <Header />
+        <AddBoard />
+        <Board />
+      </div>
     </ThemeProvider>
   );
 }
