b8b0fc1e5139e9924697567986f2143d6d5ea5a4
jonzski <jhperez4@up.edu.ph>
2025-03-22 22:41:02 +0800

    Add text header glow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
 export default function Header() {
 
-    return (
-      <header className="flex justify-center items-center p-12 gap-4">
-        <h1 className="text-5xl font-bold">YonMiru's Birthday</h1>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="flex justify-center items-center p-12 gap-4">
+      <h1
+        className="text-6xl lg:text-7xl font-black font-raleway tracking-[1px] text-transparent text-center"
+        style={{
+          WebkitTextStroke: "2px #3B82F6", // Blue stroke
+          filter: `
+      drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.5))
+      drop-shadow(0px 0px 4px rgba(59, 130, 246, 0.5))
+      drop-shadow(0px 0px 10px rgba(59, 130, 246, 0.5))
+      drop-shadow(0px 0px 50px rgba(59, 130, 246, 1))
+    `,
+        fontFamily: "Raleway",
+        }}
+      >
+        YonMiru's Birthday
+      </h1>
+    </header>
+  );
+}
